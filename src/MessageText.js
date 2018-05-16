f7c038d0ea1bae491d6f9089dc8a135b295915c1
rodolphefauquez <rodolphe.fauquez@gmail.com>
2018-05-16 17:11:48 +0900

    Update text

--- src/MessageText.js
+++ src/MessageText.js
@@ -35,7 +35,7 @@ export default class MessageText extends React.Component {
   }
 
   onPhonePress(phone) {
-    const options = ['通話', 'メッセージ', 'キャンセル'];
+    const options = ['電話', 'メッセージを送る', 'キャンセル'];
     const cancelButtonIndex = options.length - 1;
     this.context.actionSheet().showActionSheetWithOptions(
       {
